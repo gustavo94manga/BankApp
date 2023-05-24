314e34cddc3d08fabc0cd34728bb1014e76a2e6c
Angel Garza <gustavo94manga@gmail.com>
2023-05-24 16:53:19 -0500

    Create Home component, modify employee, account, bankAddress ts, create navbar component

--- ngBankApp/src/app/models/account.ts
+++ ngBankApp/src/app/models/account.ts
@@ -1,2 +1,33 @@
+import { Transaction } from "./transaction";
+import { User } from "./user";
+
 export class Account {
+  id: number;
+  accountType: string;
+  accountNumber: number;
+  balance: number;
+  accountStatus: boolean;
+  openedDate: string;
+  user: User | null;
+  transactions: Transaction[] | null;
+
+  constructor(
+    id: number = 0,
+    accountType: string = '',
+    accountNumber: number = 0,
+    balance: number = 0,
+    accountStatus: boolean = false,
+    openedDate: string = '',
+    user: User | null = null,
+    transactions: Transaction[] = [],
+  ){
+    this.id = id;
+    this.accountType = accountType;
+    this.accountNumber = accountNumber;
+    this.balance = balance;
+    this.accountStatus = accountStatus;
+    this.openedDate = openedDate;
+    this.user = user;
+    this.transactions = transactions;
+  }
 }
