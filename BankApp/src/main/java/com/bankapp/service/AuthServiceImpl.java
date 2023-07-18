0a4e36b857d71599930d6bf7ce640d139ef7f624
Angel Garza <gustavo94manga@gmail.com>
2023-07-17 20:18:06 -0500

    updated services and controllers

--- BankApp/src/main/java/com/bankapp/service/AuthServiceImpl.java
+++ BankApp/src/main/java/com/bankapp/service/AuthServiceImpl.java
@@ -1,33 +1,30 @@
 package com.bankapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import com.bankapp.entities.User;
 import com.bankapp.repository.UserRepository;
 
 @Service
 public class AuthServiceImpl implements AuthService {
-	
-	@Autowired
-	private UserRepository userRepo;
+    
+    @Autowired
+    private UserRepository userRepo;
 
-	@Autowired
-	private PasswordEncoder encoder;
-	
-	@Override
-	public User register(User user) {
-		String encrypted = encoder.encode(user.getPassword());
-		user.setPassword(encrypted);
-		user.setEnabled(true);
-		return userRepo.saveAndFlush(user);
-	}
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    
+    @Override
+    public User register(User user) {
+        String encrypted = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encrypted);
+        user.setEnabled(true);
+        return userRepo.saveAndFlush(user);
+    }
 
-	@Override
-	public User getUserByUsername(String username) {
-		// TODO Auto-generated method stub
-		return userRepo.findByUsername(username);
-	}
-
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepo.findByUsername(username);
+    }
 }
